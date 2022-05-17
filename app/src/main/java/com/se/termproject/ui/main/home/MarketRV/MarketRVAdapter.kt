f5ce7803c8ac116030af1431d2c7fd417497bd18
Cobboong <ddilong0216@naver.com>
2022-05-17 16:31:36 +0900

    Feat : made HomeFragment

---
+++ app/src/main/java/com/se/termproject/ui/main/home/MarketRV/MarketRVAdapter.kt
@@ -0,0 +1,38 @@
+package com.se.termproject.ui.main.home.MarketRV
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.se.termproject.databinding.ItemMarketBinding
+
+class MarketRVAdapter(private val marketList: ArrayList<Market>): RecyclerView.Adapter<MarketRVAdapter.ViewHolder>(){
+
+    interface MyItemClickListner{
+        fun onItemClick(market : Market)
+    }
+    private lateinit var mItemClickListner: MyItemClickListner
+
+    fun setMyItemClickListner(itemClickListner: MyItemClickListner){
+        mItemClickListner = itemClickListner
+    }
+
+    override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): MarketRVAdapter.ViewHolder {
+        val binding: ItemMarketBinding = ItemMarketBinding.inflate((LayoutInflater.from(viewGroup.context)))
+
+        return ViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: MarketRVAdapter.ViewHolder, position: Int) {
+       holder.bind(marketList[position])
+        holder.itemView.setOnClickListener { mItemClickListner.onItemClick(marketList[position]) }
+    }
+
+    override fun getItemCount(): Int = marketList.size
+
+    inner class ViewHolder(val binding: ItemMarketBinding): RecyclerView.ViewHolder(binding.root){
+        fun bind(market : Market){
+            binding.itemNameTv.text = market.name
+            binding.itemMarketIv.setImageResource(market.coverImg!!)
+        }
+    }
+}
